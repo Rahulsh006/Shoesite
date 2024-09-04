4ad6bfc80f67ad6ca3cd5b824200fb9ff9457158
Rahul S H <rahul.sh@ecsfin.com>
2024-09-04 17:00:50 +0530

    carousel main2

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     var showMoreBtn = document.getElementById('show-more-btn');
-//     var productList = document.getElementById('product-list');
-
-//     showMoreBtn.addEventListener('click', function() {
-//         if (productList.style.display === "none" || productList.style.display === "") {
-//             productList.style.display = "grid";  // Adjust according to your layout
-//             showMoreBtn.textContent = "LOAD LESS PRODUCTS";
-//         } else {
-//             productList.style.display = "none";
-//             showMoreBtn.textContent = "LOAD MORE PRODUCTS";
-//         }
-//     });
-// });
-
+// carousel main banner
 
 let nextButton = document.getElementById('next');
 let prevButton = document.getElementById('prev');
@@ -56,3 +42,6 @@ seeMoreButtons.forEach((button) => {
 backButton.onclick = function(){
     carousel.classList.remove('showDetail');
 }
+
+
+
